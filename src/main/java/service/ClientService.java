2396b40a6acef13b18aa02e8351f59957f7b1684
ASUS_ALEX\33631 <a.six109@laposte.net>
2023-03-06 16:42:39 +0100

    method supprimer client OK

--- src/main/java/service/ClientService.java
+++ src/main/java/service/ClientService.java
@@ -16,6 +16,9 @@ public class ClientService {
     public static void updateClient(EntityManager entityManager, Client client){
         ClientDAO.save(entityManager, client);
     }
+    public static void deleteClient(EntityManager entityManager, Client client){
+        ClientDAO.delete(entityManager, client);
+    }
 
 
 }
